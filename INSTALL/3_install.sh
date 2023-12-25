0eafffb78c1aedd7514f9890dbbb43c52d030192
XxiAmAnthonyxX <140768078+XxiAmAnthonyxX@users.noreply.github.com>
2023-12-25 15:40:39 -0600

    Update 3_install.sh

--- INSTALL/3_install.sh
+++ INSTALL/3_install.sh
@@ -7,6 +7,10 @@ mkdir ~/.config/dunst
 mkdir ~/.config/cava
 mkdir ~/.config/neofetch
 mkdir ~/.config/swaylock
+mkdir ~/.config/btop
+mkdir ~/.config/qimgv
+mkdir ~/.config/Thunar
+mkdir ~/.config/xfce4
 
 cp -r ~/dots/btop/btop.conf ~/.config/btop
 cp -r ~/dots/cava/config ~/.config/cava
@@ -27,14 +31,14 @@ cp -r ~/dots/thunar/uca.xml ~/.config/Thunar
 cp -r ~/dots/zsh/.p10k.zsh ~/
 cp -r ~/dots/zsh/.zshrc ~/
 
-systemctl enable NetworkManager.service
+sudo systemctl enable NetworkManager.service
 
-chmod a+wr /opt/spotify
-chmod a+wr /opt/spotify/Apps -R
+sudo chmod a+wr /opt/spotify
+sudo chmod a+wr /opt/spotify/Apps -R
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-reboot
+sudo reboot
