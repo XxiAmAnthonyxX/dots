e9fa5cbc21008261eaf944f3a14783ed04cef930
XxiAmAnthonyxX <140768078+XxiAmAnthonyxX@users.noreply.github.com>
2023-12-25 15:36:40 -0600

    Update 2_install.sh

--- INSTALL/2_install.sh
+++ INSTALL/2_install.sh
@@ -1,4 +1,4 @@
 
-yay -S firefox python-pywalfox spotify spotify-adblock-git spicetify-cli qimgv slurp grim wf-recorder thunar tumbler gvfs thunar-volman thunar-archive-plugin file-roller pavucontrol cava neofetch zsh oh-my-zsh-git zsh-theme-powerlevel10k zsh-syntax-highlighting zsh-autosuggestions btop rofi cliphist dunst udiskie python-pywal nwg-look-bin themix-full-git swww swaylock-effects-git libreoffice-still vscodium bottles-git gamemode pactl brightnessctl playerctl networkmanager network-manager-applet 
+yay -S firefox python-pywalfox spotify spotify-adblock-git spicetify-cli qimgv slurp grim wf-recorder thunar tumbler gvfs thunar-volman thunar-archive-plugin file-roller pavucontrol cava neofetch zsh oh-my-zsh-git zsh-theme-powerlevel10k zsh-syntax-highlighting zsh-autosuggestions btop rofi cliphist dunst udiskie python-pywal nwg-look-bin themix-full-git swww swaylock-effects-git libreoffice-still vscodium bottles-git gamemode pulseaudio-nextsink brightnessctl playerctl networkmanager network-manager-applet 
 
 sudo reboot
