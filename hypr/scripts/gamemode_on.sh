30c4407aa9c3aa5bbe85a63c9a84a8574c80c622
XxiAmAnthonyxX <140768078+XxiAmAnthonyxX@users.noreply.github.com>
2023-12-30 17:58:12 +0000

    Update gamemode_on.sh

--- hypr/scripts/gamemode_on.sh
+++ hypr/scripts/gamemode_on.sh
@@ -1,7 +1,9 @@
 
 hyprctl --batch "\
 keyword animations:enabled false;\
-keyword decoration:blur:enabled false"
+keyword decoration:blur:enabled false;\
+keyword decoration:drop_shadow false;\
+keyword decoration:rounding 0"
 exit
 
 hyprctl reload
