ec69064459d5ebbf3571e22d6f722d11fdbdbea9
XxiAmAnthonyxX <140768078+XxiAmAnthonyxX@users.noreply.github.com>
2023-12-30 17:58:01 +0000

    Update gamemode_off.sh

--- hypr/scripts/gamemode_off.sh
+++ hypr/scripts/gamemode_off.sh
@@ -1,7 +1,9 @@
 
 hyprctl --batch "\
 keyword animations:enabled true;\
-keyword decoration:blur:enabled true"
+keyword decoration:blur:enabled true;\
+keyword decoration:drop_shadow true;\
+keyword decoration:rounding 0"
 exit
 
 hyprctl reload
