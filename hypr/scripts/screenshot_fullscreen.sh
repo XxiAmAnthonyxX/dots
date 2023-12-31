1e7a675bbe2acde76db6278c5a5ad1db9dd4c271
XxiAmAnthonyxX <140768078+XxiAmAnthonyxX@users.noreply.github.com>
2023-12-31 16:35:09 +0000

    Update screenshot_fullscreen.sh

--- hypr/scripts/screenshot_fullscreen.sh
+++ hypr/scripts/screenshot_fullscreen.sh
@@ -3,4 +3,4 @@ screenshot='./screenshots/screenshot_'"$(date '+%Y_%m_%_d..%H.%M.%S')"'.png'
 
 grimblast save screen './screenshots/screenshot_'"$(date '+%Y_%m_%_d..%H.%M.%S')"'.png'
 
-swappy -f $screenshot
+nomacs $screenshot
