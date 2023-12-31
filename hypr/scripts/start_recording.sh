3822c20e2948aeba0b349683e8035ec008ad8a65
XxiAmAnthonyxX <140768078+XxiAmAnthonyxX@users.noreply.github.com>
2023-12-31 02:24:40 +0000

    Update start_recording.sh

--- hypr/scripts/start_recording.sh
+++ hypr/scripts/start_recording.sh
@@ -1,3 +1,8 @@
+
 cd ~/recordings
 
+recording='./recording_'"$(date '+%Y_%m_%_d..%H.%M.%S')"'.mp4'
+
 wf-recorder -a -g "$(slurp)" -f './recording_'"$(date '+%Y_%m_%_d..%H.%M.%S')"'.mp4'
+
+swappy -f $recording
