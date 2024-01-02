0a31d2bdc4c33b72def6fa7709f7e182a6e24767
XxiAmAnthonyxX <140768078+XxiAmAnthonyxX@users.noreply.github.com>
2024-01-02 15:10:21 +0000

    Update screenshot_area.sh

--- hypr/scripts/screenshot_area.sh
+++ hypr/scripts/screenshot_area.sh
@@ -3,4 +3,4 @@ screenshot='./screenshots/screenshot_'"$(date '+%Y_%m_%_d..%H.%M.%S')"'.png'
 
 grimblast save area './screenshots/screenshot_'"$(date '+%Y_%m_%_d..%H.%M.%S')"'.png'
 
-nomacs $screenshot
+qimgv $screenshot
