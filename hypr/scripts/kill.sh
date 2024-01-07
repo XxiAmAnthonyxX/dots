8a2572cb1d35ef6671c5b79ea7fc79d2315bf9cb
XxiAmAnthonyxX <140768078+XxiAmAnthonyxX@users.noreply.github.com>
2024-01-07 14:11:10 +0000

    Update kill.sh

--- hypr/scripts/kill.sh
+++ hypr/scripts/kill.sh
@@ -1,16 +1,4 @@
-killall swww-daemon
 killall dunst
 killall kitty
-killall cava
-killall btop
-killall qimgv
 killall firefox
-#killall spotify
-killall thunar
-killall codium
-killall pavucontrol
-killall soffice.bin oosplash
-killall rofi
 killall nwg-look
-killall themix-gui
-killall bottles
