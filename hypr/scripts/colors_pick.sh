2c875b52ecbafdab5d8cab4d9a53246f0a0f9816
XxiAmAnthonyxX <140768078+XxiAmAnthonyxX@users.noreply.github.com>
2024-01-07 14:09:37 +0000

    Update colors_pick.sh

--- hypr/scripts/colors_pick.sh
+++ hypr/scripts/colors_pick.sh
@@ -17,12 +17,12 @@ cp -r $HOME/.cache/wal/colors-cava $HOME/.config/cava
 cd $HOME/.config/cava
 mv colors-cava config
 
-#cp -r $HOME/.cache/wal/colors-spicetify.ini $HOME/.config/spicetify/Themes/Pywal
-#cd $HOME/.config/spicetify/Themes/Pywal
-#mv colors-spicetify.ini color.ini
-#spicetify backup apply
-#spicetify config current_theme Pywal
-#spicetify apply
+cp -r $HOME/.cache/wal/colors-spicetify.ini $HOME/.config/spicetify/Themes/Pywal
+cd $HOME/.config/spicetify/Themes/Pywal
+mv colors-spicetify.ini color.ini
+spicetify backup apply
+spicetify config current_theme Pywal
+spicetify apply
 
 cp -r $HOME/.cache/wal/colors-dunst $HOME/.config/dunst
 cd $HOME/.config/dunst
@@ -37,3 +37,5 @@ cd $HOME/.config/qimgv
 mv colors-qimgv theme.conf
 
 nwg-look
+
+~/.config/hypr/scripts/launch.sh
