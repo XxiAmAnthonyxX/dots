afda47460fd69b0347001bdb32c82403dbd9ba20
XxiAmAnthonyxX <140768078+XxiAmAnthonyxX@users.noreply.github.com>
2024-02-01 14:07:19 -0600

    Delete hypr/scripts/recording_start.sh

--- hypr/scripts/recording_start.sh
+++
@@ -1,8 +0,0 @@
-
-cd ~/recordings
-
-recording='./recording_'"$(date '+%Y_%m_%_d..%H.%M.%S')"'.mp4'
-
-wf-recorder -a -g "$(slurp)" -f './recording_'"$(date '+%Y_%m_%_d..%H.%M.%S')"'.mp4'
-
-~/.config/hypr/scripts/end_recording.sh "$recording"
