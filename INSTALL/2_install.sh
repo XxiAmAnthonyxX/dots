76612aa183f7ad5beea29748b6b5c80ec5da213a
XxiAmAnthonyxX <140768078+XxiAmAnthonyxX@users.noreply.github.com>
2024-02-01 12:53:06 -0600

    Update 2_install.sh

--- INSTALL/2_install.sh
+++ INSTALL/2_install.sh
@@ -1,4 +1,4 @@
 
 yay -S firefox python-pywalfox spotify spicetify-cli qimgv grimblast wf-recorder thunar tumbler gvfs thunar-volman thunar-archive-plugin file-roller pavucontrol cava neofetch zsh oh-my-zsh-git zsh-theme-powerlevel10k zsh-syntax-highlighting zsh-autosuggestions btop rofi cliphist dunst udiskie python-pywal nwg-look-bin themix-full-git swww swaylock-effects-git libreoffice-still vscodium-electron lib32-nvidia-utils bottles-git gamemode pulseaudio-nextsink brightnessctl playerctl networkmanager network-manager-applet ttf-maple
 
-#sudo reboot
+sudo reboot
