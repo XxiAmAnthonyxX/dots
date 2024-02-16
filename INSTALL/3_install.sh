95d0c49bbdcb94b13262fb5ce564be5ff96ace1c
XxiAmAnthonyxX <140768078+XxiAmAnthonyxX@users.noreply.github.com>
2024-02-15 19:31:57 -0600

    Update 3_install.sh

--- INSTALL/3_install.sh
+++ INSTALL/3_install.sh
@@ -39,6 +39,7 @@ sudo systemctl enable NetworkManager.service
 
 sudo chmod a+wr /opt/spotify
 sudo chmod a+wr /opt/spotify/Apps -R
+spicetify backup apply
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
