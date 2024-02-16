ac5cf0a408264f9289f0d10b72783fe9a5cbeb78
XxiAmAnthonyxX <140768078+XxiAmAnthonyxX@users.noreply.github.com>
2024-02-15 21:25:16 -0600

    Update wallpaper.sh

--- rofi/wallpaper.sh
+++ rofi/wallpaper.sh
@@ -22,6 +22,43 @@ done
 # Select a picture with rofi
 wall_selection=$(find "${wall_dir}" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) -exec basename {} \; | sort | while read -r A ; do  echo -en "$A\x00icon\x1f""${cacheDir}"/"$A\n" ; done | $rofi_command)
 
-~/.config/hypr/scripts/theme.sh "$wall_selection"
+killall dunst
+pkill -USR2 cava
 
-exit 0
+swww init
+swww img ~/dots/wallpapers/$wall_selection
+rm -rf $HOME/.cache/swww
+
+#theme=$(ls -1 ~/.themes | grep "oomox" | rofi -dmenu -replace -config ~/.config/rofi/config-wallpaper.rasi)
+#icons=$(ls -1 ~/.icons | grep "oomox" | rofi -dmenu -replace -config ~/.config/rofi/config-wallpaper.rasi)
+
+#gsettings set org.gnome.desktop.interface gtk-theme "$theme"
+#gsettings set org.gnome.desktop.interface icon-theme "$icons"
+
+wal -c
+wal -i ~/dots/wallpapers/$wall_selection
+
+cp ~/dots/wallpapers/$wall_selection ~/.cache/current_wallpaper.rasi
+
+pywalfox update
+
+cp -r $HOME/.cache/wal/colors-cava $HOME/.config/cava
+cd $HOME/.config/cava
+mv colors-cava config
+
+cp -r $HOME/.cache/wal/colors-spicetify.ini $HOME/.config/spicetify/Themes/Pywal
+cd $HOME/.config/spicetify/Themes/Pywal
+mv colors-spicetify.ini color.ini
+spicetify apply
+
+cp -r $HOME/.cache/wal/colors-dunst $HOME/.config/dunst
+cd $HOME/.config/dunst
+mv colors-dunst dunstrc
+
+cp -r $HOME/.cache/wal/colors-swaylock $HOME/.config/swaylock
+cd $HOME/.config/swaylock
+mv colors-swaylock config
+
+cp -r $HOME/.cache/wal/colors-qimgv $HOME/.config/qimgv
+cd $HOME/.config/qimgv
+mv colors-qimgv theme.conf
