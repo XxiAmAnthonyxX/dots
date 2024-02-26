db59d80fc35f0eaaf4ad2c85a18bfa4d125a2649
XxiAmAnthonyxX <140768078+XxiAmAnthonyxX@users.noreply.github.com>
2024-02-25 18:39:39 -0600

    Update gamemode_on.sh

--- hypr/scripts/gamemode_on.sh
+++ hypr/scripts/gamemode_on.sh
@@ -1,9 +1,14 @@
-
-hyprctl --batch "\
-keyword animations:enabled false;\
-keyword decoration:blur:enabled false;\
-keyword decoration:drop_shadow false;\
-keyword decoration:rounding 0"
-exit
-
+#!/usr/bin/env sh
+HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
+if [ "$HYPRGAMEMODE" = 1 ] ; then
+    hyprctl --batch "\
+        keyword animations:enabled 0;\
+        keyword decoration:drop_shadow 0;\
+        keyword decoration:blur:enabled 0;\
+        keyword general:gaps_in 0;\
+        keyword general:gaps_out 0;\
+        keyword general:border_size 1;\
+        keyword decoration:rounding 0"
+    exit
+fi
 hyprctl reload
