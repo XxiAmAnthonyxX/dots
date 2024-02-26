34028fbb805c6d00510377de49683413f81ed9bc
XxiAmAnthonyxX <140768078+XxiAmAnthonyxX@users.noreply.github.com>
2024-02-25 23:23:19 -0600

    Update gamemode_off.sh

--- hypr/scripts/gamemode_off.sh
+++ hypr/scripts/gamemode_off.sh
@@ -1,14 +1,13 @@
-#!/usr/bin/env sh
-HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
-if [ "$HYPRGAMEMODE" = 1 ] ; then
-    hyprctl --batch "\
-        keyword animations:enabled 1;\
-        keyword decoration:drop_shadow 1;\
-        keyword decoration:blur:enabled 1;\
-        keyword general:gaps_in 5;\
-        keyword general:gaps_out 20;\
-        keyword general:border_size 5;\
-        keyword decoration:rounding 25"
-    exit
-fi
+hyprctl --batch "\
+    keyword animations:enabled 1;\
+    keyword decoration:drop_shadow 1;\
+    keyword decoration:blur:enabled 1;\
+    keyword general:gaps_in 5;\
+    keyword general:gaps_out 20;\
+    keyword general:border_size 5;\
+    keyword decoration:rounding 25;\
+    keyword decoration:active_opacity 0.95;\
+    keyword decoration:inactive_opacity 0.95"
+exit
+
 hyprctl reload
